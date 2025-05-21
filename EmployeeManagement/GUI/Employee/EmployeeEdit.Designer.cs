db697a1e92332a5e5a98b2ed30f810a9ca90d3cf
khongvan-dt <khongvandt14082004@gmail.com>
2025-05-21 17:05:00 +0700

    add code để pull code mời

--- EmployeeManagement/GUI/Employee/EmployeeEdit.Designer.cs
+++ EmployeeManagement/GUI/Employee/EmployeeEdit.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace EmployeeManagement.GUI.Employee
+﻿using System;
+using System.Windows.Forms;
+
+namespace EmployeeManagement.GUI.Employee
 {
     partial class EmployeeEdit
     {
@@ -31,7 +34,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ProjectEdit";
+            this.Text = "Chỉnh sửa nhân viên";
         }
 
         #endregion
